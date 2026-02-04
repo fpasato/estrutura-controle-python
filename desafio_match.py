cca4de232d32d4c38527d7931be2c9be8bdb3390
fpasato <fp.asato.dev@gmail.com>
2026-02-04 00:54:00 -0300

    desafio

---
+++ desafio_match.py
@@ -0,0 +1,24 @@
+print(" Bem-vindo ao sistema de atendimento! ".center(50,'='))
+print()
+
+while True:
+    opcao = int(input("Digite a opção desejada: \n[1] Falar com atendente\n[2] Segunda via de boleto\n[3] Cancelar serviço\n[4] Informações sobre planos\n[5] Sair\n(apenas números) "))
+    match opcao:
+        case 1:
+            print("Aguarde... ")
+            break
+        case 2:
+            print("Imprimindo boleto...")
+            break
+        case 3:
+            print('Cancelando serviço...')
+            break
+        case 4:
+            print('Informações sobre planos...')
+            break
+        case 5:
+            print('Saindo...')
+            break
+        case _:
+            print('Opção inválida, tente novamente!')
+            continue
