dc707d0541bbd5492f6e1ca6def6d3d30075aae5
Fernando Asato <fp.asato.dev@gmail.com>
2026-02-04 20:12:08 -0300

    desafio match

--- desafio_match.py
+++ desafio_match.py
@@ -3,29 +3,25 @@ print()
 
 while True:
 
-    try:
-        opcao = int(input("Digite a opção desejada: \n[1] Falar com atendente\n[2] Segunda via de boleto\n[3] Cancelar serviço\n[4] Informações sobre planos\n[5] Sair\n"))
+    opcao = input("Digite a opção desejada: \n[1] Falar com atendente\n[2] Segunda via de boleto\n[3] Cancelar serviço\n[4] Informações sobre planos\n[5] Sair\n")
 
-        match opcao:
-            case 1:
-                print("Aguarde... ")
-                break
-            case 2:
-                print("Imprimindo boleto...")
-                break
-            case 3:
-                print('Cancelando serviço...')
-                break
-            case 4:
-                print('Informações sobre planos...')
-                break
-            case 5:
-                print('Saindo...')
-                break
-            case _:
-                print('Opção inválida, tente novamente!')
-                continue
-    except ValueError:
-        print()
-        print("Digite apenas números")
-        print()
+    match opcao:
+        case '1':
+            print("Aguarde... ")
+            break
+        case '2':
+            print("Imprimindo boleto...")
+            break
+        case '3':
+            print('Cancelando serviço...')
+            break
+        case '4':
+            print('Informações sobre planos...')
+            break
+        case '5':
+            print('Saindo...')
+            break
+        case _:
+            print('Opção inválida, tente novamente!')
+            continue
+
